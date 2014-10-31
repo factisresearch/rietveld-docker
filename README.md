255c3797e568cb2e6c437c3091782ad29f37ac60
Stefan Wehr <mail@stefanwehr.de>
2014-10-31 12:10:30 +0100

    version of rietveld as deployed for long time on darcs.factisresearch.com

---
+++ README.md
@@ -0,0 +1,2 @@
+A [dockerized](http://docker.io) version of
+[rietveld](https://code.google.com/p/rietveld), a code-reviewing tool.
--- rietveld/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /apps/rietveld && ./manage.py runserver 8000
-#cd /apps/rietveld && ./manage.py runfcgi host=127.0.0.1 port=8000
